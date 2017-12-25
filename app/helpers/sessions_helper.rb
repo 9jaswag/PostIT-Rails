c48665a3ecd84dad5e2ce260862ff31a1b0fd7dc
Chuks Opia <chuks24ng@yahoo.co.uk>
2017-12-25 16:56:09 +0100

    add currentUser and loggedin helper and toggle menu icon based on loggedin status

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,17 @@
 module SessionsHelper
-  
+
   # Log in a given user
   def log_in(user)
     session[:user_id] = user.id
+  end
+
+  # Find current user
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # Return true if user is logged in or false if otherwise
+  def logged_in?
+    !current_user.nil?
   end
 end
