a8dc8c73ccf77278bbc6710b023e2dedfb24c5db
Chuks Opia <chuks24ng@yahoo.co.uk>
2017-12-25 16:25:43 +0100

    find and authenticate user and display flash error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username_signin].downcase)
+    if user && user.authenticate(params[:session][:password_signin])
+      redirect_to user_path(user[:id])
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      redirect_to users_path      
+    end
+  end
+
+  def destroy
+  end
 end
