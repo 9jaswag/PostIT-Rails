588191792af6c00ab0dc0adbf8d2b1845526fffa
Chuks Opia <chuks24ng@yahoo.co.uk>
2017-12-25 15:43:16 +0100

    downcase username before saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
+  before_save { self.username = username.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :username, presence: true, length: { maximum: 20 },
                         uniqueness: true
