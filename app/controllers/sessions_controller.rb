160428a701dbc4a10e67f6607e225380e16616a4
Chuks Opia <chuks24ng@yahoo.co.uk>
2017-12-25 16:36:09 +0100

    use login module in session controller to login user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:session][:username_signin].downcase)
     if user && user.authenticate(params[:session][:password_signin])
-      redirect_to user_path(user[:id])
+      log_in user
+      redirect_to user
     else
       flash[:danger] = 'Invalid email/password combination'
       redirect_to users_path      
