e8f838ec2cdcc61a7e833247bd93e765f7f9e042
Chuks Opia <chuks24ng@yahoo.co.uk>
2017-12-27 18:42:38 +0100

    create group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,29 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group[:owner] = current_user.username
+
+    if @group.save
+      flash[:success] = "Signup successful! Welcome!"
+      redirect_to @group
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def group_params
+      params.require(:group).permit(:name, :description)
+    end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,5 @@
 class Group < ApplicationRecord
-  # before_save { self.owner = owner of the group }
-  validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 30 }
   validates :owner, presence: true
   validates :description, presence: true, length: { maximum: 60 }
 end
