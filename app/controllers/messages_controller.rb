e6243431ab452b470d020c4683b90dff7544ac56
Chuks Opia <chuks24ng@yahoo.co.uk>
2017-12-28 17:51:07 +0100

    create and view message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,23 +3,26 @@ class MessagesController < ApplicationController
     @messages = Message.all    
   end
 
+  def show
+    @message = Message.find(params[:id])
+  end
+
   def new
     @message = Message.new
   end
 
   def create
-    @message = Message.new(message_params)
-    render plain: params
-    # respond_to do |format|
-    #   if @message.save
-    #     flash[:notice] = 'Message was successfully created.'
-    #     format.html { redirect_to(@message) }
-    #     format.xml { render xml: @message, status: :created, location: @message }
-    #   else
-    #     format.html { render action: "new" }
-    #     format.xml { render xml: @message.errors, status: :unprocessable_entity }
-    #   end
-    # end
+    @message = current_user.messages.new(message_params)
+    @message[:readby] << current_user.username
+    @message[:group_id] = params[:group_id]
+    respond_to do |format|
+      if @message.save
+        flash[:success] = 'Message was successfully created.'
+        format.html { redirect_to group_message_path(id: @message[:id]) }
+      else
+        format.html { render action: "new" }
+      end
+    end
   end
   
   private
--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,8 @@ class Message < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }
   validates :body, presence: true
   validates :readby, presence: true
+  validates :group_id, presence: true
+  validates :user_id, presence: true
   validates :priority, presence: true, inclusion: { in: %w(normal urgent critical),
                                       message: "%{value} is not a valid priority" }
 end
