e7ed6c0b5d62f93541e7763ff9ee6548ebae35cd
Chuks Opia <chuks24ng@yahoo.co.uk>
2017-12-28 13:50:53 +0100

    run message model migration

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :logged_in_user, only: [:index, :show, :new, :create]
+  before_action :logged_in_user
   def index
     @groups = Group.all
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171227175924) do
+ActiveRecord::Schema.define(version: 20171228084521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,19 @@ ActiveRecord::Schema.define(version: 20171227175924) do
     t.index ["name"], name: "index_groups_on_name", unique: true
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string "title"
+    t.text "readby", default: [], array: true
+    t.text "body"
+    t.text "priority", default: "normal"
+    t.bigint "user_id"
+    t.bigint "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_messages_on_group_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -50,4 +63,6 @@ ActiveRecord::Schema.define(version: 20171227175924) do
 
   add_foreign_key "group_members", "groups"
   add_foreign_key "group_members", "users"
+  add_foreign_key "messages", "groups"
+  add_foreign_key "messages", "users"
 end
