ba1e4fd041bafd1754198618dd4a809642440b63
Chuks Opia <chuks24ng@yahoo.co.uk>
2017-12-28 14:36:03 +0100

    create message controller and add validations to message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :body, presence: true
+  validates :readby, presence: true
+  validates :priority, presence: true, inclusion: { in: %w(normal urgent critical),
+                                      message: "%{value} is not a valid priority" }
 end
