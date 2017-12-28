b5e5a0580bb03863733983a20053f278604dadf8
Chuks Opia <chuks24ng@yahoo.co.uk>
2017-12-28 09:47:32 +0100

    add priority default in message model

--- db/migrate/20171228084521_create_messages.rb
+++ db/migrate/20171228084521_create_messages.rb
@@ -4,7 +4,7 @@ class CreateMessages < ActiveRecord::Migration[5.1]
       t.string :title
       t.text :readby, array: true, default: []
       t.text :body
-      t.text :priority
+      t.text :priority, default: 'normal'
       t.references :user, foreign_key: true
       t.references :group, foreign_key: true
 
