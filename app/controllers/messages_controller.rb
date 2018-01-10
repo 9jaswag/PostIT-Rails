e612c171a12b20bdb4e635d93b4dce76644b6a38
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-01-10 14:55:04 +0100

    first message auth implementation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,10 @@ class MessagesController < ApplicationController
 
   def show
     is_group_member(params[:group_id])
+    # confirm if message belongs to group
+    # Group.joins(:messages).where('group_id = ?, messages.id =?', 2, 2)
     @message = Message.find(params[:id])
+    redirect_to group_path(params[:group_id]) unless Group.find(params[:group_id]).messages.include?(@message)
   rescue StandardError
     flash[:danger] = "Message does not exist"
     redirect_to group_path(params[:group_id])
