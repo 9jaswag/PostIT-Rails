5a5a8bbf581e64f1c33f1cce8bf2835ab6119451
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-01-13 11:48:27 +0100

    create and run activation column migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171228084521) do
+ActiveRecord::Schema.define(version: 20180113104734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,9 @@ ActiveRecord::Schema.define(version: 20171228084521) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_time"
     t.index ["email"], name: "index_users_on_email"
     t.index ["phone"], name: "index_users_on_phone"
     t.index ["username"], name: "index_users_on_username"
