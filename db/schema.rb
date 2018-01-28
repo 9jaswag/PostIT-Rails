19908e55d67b1aaa586875690366afccf0ceb9d8
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-01-28 11:44:40 +0100

    remove password column form db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180113104734) do
+ActiveRecord::Schema.define(version: 20180128104329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 20180113104734) do
     t.datetime "reset_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password"
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_time"
