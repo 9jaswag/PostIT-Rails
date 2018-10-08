bd54cfb3d77517dfa2df89bcc6518e5ae5fdb64e
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-10-08 21:30:09 +0100

    - add users spec helper - update users factory - add more user controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Users Controller', type: :request do
   # initialize test data
   let!(:user) { create(:user) }
-  # let!(:new_user) { build(:user) }
   # let(:valid_attributes) do
   #   attributes_for(:new_user, password_confirmation: user.password)
   # end
 
-  # #new
+  # new action
   describe '#new' do
       it 'returns a successful response' do
         get new_user_path
@@ -16,7 +15,7 @@ RSpec.describe 'Users Controller', type: :request do
       end
   end
 
-  # #show
+  # show action
   describe '#show' do
     context 'when user is not signed in' do
       before { get user_path(id: user.id) }
@@ -24,10 +23,29 @@ RSpec.describe 'Users Controller', type: :request do
         expect(response).to redirect_to users_path
       end
     end
+
+    context 'when user is signed in' do
+      before do
+        sign_in user
+      end
+      it 'redirects the users page' do
+        get user_path(id: user.id)
+        expect(response.body).to include "@#{user.username}"
+      end
+    end
   end
 
-  # #create
+  # create action
   describe '#create' do
+    context 'when user parameters is invalid' do
+      before do
+        post users_path(user: { phone: '12345678901' })
+      end
+      it 'renders the index page' do
+        expect(response).to render_template("index")
+      end
+    end
+
     context 'when user parameters is valid' do
       before do
         post users_path(user: {
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do
   factory :user do
-    username { Faker::Name.unique.name }
+    username { Faker::Name.unique.first_name[1..6] }
     email { Faker::Internet.unique.email }
     password { Faker::Internet.password }
     phone { '01234567890' }
+    activated { true }
+    activated_time { Time.zone.now }
   end
 end
