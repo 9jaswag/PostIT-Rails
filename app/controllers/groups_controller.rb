9c712bb63b6f46fceef382767fd0d9ac51c7cd9e
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-10-11 17:33:35 +0100

    chore: write controller tests
    - add more group controller tests

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -83,11 +83,10 @@ class GroupsController < ApplicationController
       member = group_member(params[:user_id], params[:group_id])
       if member.exists?
         if member[0].group[:owner] == member[0].user[:username] # if group owner is trying to remove self
-          @message = "Why you wanna do like that? You can't leave your group"
+          @message = "Why you wanna do like that? You cant leave your group"
         else
           if member.destroy(member[0].id)
             @message = 'User has been removed from group'
-            puts @message
             respond_to do |format|
               format.js
             end
